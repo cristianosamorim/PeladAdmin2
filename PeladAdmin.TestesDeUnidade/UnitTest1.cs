dd485a051206a2cc0d7a602f6c7c4f51ae096776
Palladino <marcelopalladino@msn.com>
2015-03-01 22:25:20 -0300

    Concluindo a versão mínima do domínio, finalizando testes iniciais e fazendo algumas refatorações/movimentações no código.

--- PeladAdmin.TestesDeUnidade/UnitTest1.cs
+++ PeladAdmin.TestesDeUnidade/UnitTest1.cs
@@ -29,8 +29,8 @@ namespace PeladAdmin.TestesDeUnidade
             Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
 
             //Act
-            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
-            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
 
             //Assert
             Assert.IsTrue(time.Caixas.Count() == 2);
@@ -56,18 +56,17 @@ namespace PeladAdmin.TestesDeUnidade
             //Arrange
             Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
 
-            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
-            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
             time.CriarPeladeiro(new Peladeiro("João Português", new DateTime(2015, 1, 1)));
             time.CriarPeladeiro(new Peladeiro("Cidão", new DateTime(2015, 1, 1)));
 
             //Act
             ServicoDePagamento servicoDePagamento = new ServicoDePagamento();
-            var identificadorDoPagamento = servicoDePagamento.Pagar(time, time.Caixas.First(), time.Peladeiros.First(), new DateTime(2015,1,1), 10);
+            var resultado = servicoDePagamento.Pagar(time.Caixas.First(), new DateTime(2015,1,1), 10);
 
             //Assert
-            Assert.IsTrue(identificadorDoPagamento.Id > 0);
-            Assert.IsTrue(identificadorDoPagamento.Valor == -10);
+            Assert.IsTrue(resultado);
         }
 
         [TestMethod]
@@ -76,32 +75,85 @@ namespace PeladAdmin.TestesDeUnidade
             //Arrange
             Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
 
-            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
-            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new Vigencia(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarCaixa(new Caixa("Festa final de ano 2015", 10, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
             time.CriarPeladeiro(new Peladeiro("João Português", new DateTime(2015, 1, 1)));
             time.CriarPeladeiro(new Peladeiro("Cidão", new DateTime(2015, 1, 1)));
 
             //Act
             ServicoDeRecebimento servicoDeRecebimento = new ServicoDeRecebimento();
-            var identificadorDoRecebimento = servicoDeRecebimento.Receber(time, time.Caixas.First(), time.Peladeiros.First(), new DateTime(2015, 1, 1), 10);
+            var resultado = servicoDeRecebimento.Receber(time.Caixas.First(), time.Peladeiros.First(), new DateTime(2015, 1, 1), 10);
 
             //Assert
-            Assert.IsTrue(identificadorDoRecebimento.Id > 0);
+            Assert.IsTrue(resultado);
         }
 
         [TestMethod]
-        public void ComoPresidenteQueroVisualizarOsPeladeirosInadimplentes()
+        public void ComoPresidenteQueroVisualizarOsPeladeirosInadimplentesQuandoJogadorComecouDepoisDoInicioDaVigenciaDoCaixa()
         {
             //Arrange
             Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarPeladeiro(new Peladeiro("João Português", new DateTime(2015, 2, 1)));
+            time.CriarPeladeiro(new Peladeiro("Cidão", new DateTime(2015, 1, 1)));
+
+            var caixa = time.Caixas.First();
+            caixa.FazerLancamentoDeRecebimento(time.Peladeiros.First(), DateTime.Now, 160); //o jogador começou em fevereiro
 
             //Act
             ServicoDeConsultaDeInadimplentes servicoDeConsultaDeInadimplentes = new ServicoDeConsultaDeInadimplentes();
-            IDictionary<Caixa, IEnumerable<Peladeiro>> inadimplentesPorCaixa = servicoDeConsultaDeInadimplentes.Consultar(time, new DateTime(2015,3,10));
+            IDictionary<Caixa, IEnumerable<Peladeiro>> inadimplentesPorCaixa = servicoDeConsultaDeInadimplentes.Consultar(time, new DateTime(2015,5,10));
 
             //Assert
-
             Assert.IsTrue(inadimplentesPorCaixa.Count == 1);
+            Assert.IsTrue(inadimplentesPorCaixa.First().Value.Count() == 1);
+        }
+
+        [TestMethod]
+        public void ComoPresidenteQueroVisualizarOsPeladeirosInadimplentesQuandoJogadorComecouAntesDoInicioDaVigenciaDoCaixa()
+        {
+            //Arrange
+            Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(new DateTime(2015, 1, 1), new DateTime(2015, 12, 31))));
+            time.CriarPeladeiro(new Peladeiro("João Português", new DateTime(2015, 1, 1)));
+            time.CriarPeladeiro(new Peladeiro("Cidão", new DateTime(2015, 1, 1)));
+
+            var caixa = time.Caixas.First();
+            caixa.FazerLancamentoDeRecebimento(time.Peladeiros.First(), DateTime.Now, 200); 
+
+            //Act
+            ServicoDeConsultaDeInadimplentes servicoDeConsultaDeInadimplentes = new ServicoDeConsultaDeInadimplentes();
+            IDictionary<Caixa, IEnumerable<Peladeiro>> inadimplentesPorCaixa = servicoDeConsultaDeInadimplentes.Consultar(time, new DateTime(2015, 5, 10));
+
+            //Assert
+            Assert.IsTrue(inadimplentesPorCaixa.Count == 1);
+            Assert.IsTrue(inadimplentesPorCaixa.First().Value.Count() == 1);
+        }
+
+        [TestMethod]
+        public void ComoPresidenteQueroVisualizarOsPeladeirosInadimplentesQuandoTodosPagaramOQueDeviam()
+        {
+            //Arrange
+            const decimal mensalidade = 40;
+            DateTime dataInicioDaVigenciaDoCaixa = new DateTime(2015, 1, 1);
+            DateTime dataInicioDeReferenciaParaConsultaDeInadimplentes = new DateTime(2015, 5, 10);
+
+
+            Time time = new Time("Amigos do Society", new DateTime(2000, 1, 1), new Presida("Marcelo Palladino"));
+            time.CriarCaixa(new Caixa("Mensalidades 2015", 40, new VigenciaDoCaixa(dataInicioDaVigenciaDoCaixa, new DateTime(2015, 12, 31))));
+            time.CriarPeladeiro(new Peladeiro("João Português", new DateTime(2015, 1, 1)));
+            time.CriarPeladeiro(new Peladeiro("Cidão", new DateTime(2015, 1, 1)));
+
+            var caixa = time.Caixas.First();
+            caixa.FazerLancamentoDeRecebimento(time.Peladeiros.ElementAt(0), DateTime.Now, 200);
+            caixa.FazerLancamentoDeRecebimento(time.Peladeiros.ElementAt(1), DateTime.Now, 200);
+
+            //Act
+            ServicoDeConsultaDeInadimplentes servicoDeConsultaDeInadimplentes = new ServicoDeConsultaDeInadimplentes();
+            IDictionary<Caixa, IEnumerable<Peladeiro>> inadimplentesPorCaixa = servicoDeConsultaDeInadimplentes.Consultar(time, dataInicioDeReferenciaParaConsultaDeInadimplentes);
+
+            //Assert
+            Assert.IsTrue(inadimplentesPorCaixa.Count == 0);
         }
     }
 
@@ -109,25 +161,45 @@ namespace PeladAdmin.TestesDeUnidade
     {
         public IDictionary<Caixa, IEnumerable<Peladeiro>> Consultar(Time time, DateTime dataDeReferencia)
         {
-            return null; //TODO:implementar
+            var resultado = new Dictionary<Caixa, IEnumerable<Peladeiro>>();
+
+            foreach (var caixa in time.Caixas)
+            {
+                var peladeirosInadimplementesNesteCaixa = new List<Peladeiro>();
+
+                foreach (var peladeiro in time.Peladeiros)
+                {
+                    var dataInicialParaApuracaoDeInadimplencia = caixa.VigenciaDoCaixa.PegaDataInicioDeVigenciaConformeDataInicioDoPeladeiro(peladeiro);
+                    var dataFinalParaApuracaoDeInadimplencia = dataDeReferencia;
+
+                    if (caixa.CalculaValorPagoPeloPeladeiroComBaseNaDataDeReferencia(peladeiro, dataDeReferencia) < 
+                        caixa.CalculaQuantoDeveSerPagoNoPeriodo(dataInicialParaApuracaoDeInadimplencia, dataFinalParaApuracaoDeInadimplencia))
+                        peladeirosInadimplementesNesteCaixa.Add(peladeiro);
+                }
+
+                if (peladeirosInadimplementesNesteCaixa.Any())
+                    resultado.Add(caixa, peladeirosInadimplementesNesteCaixa);
+            }
+
+            return resultado;
         }
     }
 
     public class ServicoDePagamento
     {
-        public LancamentoPagamento Pagar(Time time, Caixa caixa, Peladeiro peladeiro, DateTime dataDePagamento, decimal valor)
+        public bool Pagar(Caixa caixa, DateTime dataDePagamento, decimal valor)
         {
-            LancamentoPagamento lancamento = new LancamentoPagamento(time, caixa, dataDePagamento, valor);
-            return lancamento;
+            caixa.FazerLancamentoDePagamento(dataDePagamento, valor);
+            return true;
         }
     }
 
     public class ServicoDeRecebimento
     {
-        public LancamentoRecebimento Receber(Time time, Caixa caixa, Peladeiro peladeiro, DateTime dataDeRecebimento, decimal valor)
+        public bool Receber(Caixa caixa, Peladeiro peladeiro, DateTime dataDeRecebimento, decimal valor)
         {
-            LancamentoRecebimento lancamento = new LancamentoRecebimento(time, caixa, peladeiro, dataDeRecebimento, valor);
-            return lancamento;
+            caixa.FazerLancamentoDeRecebimento(peladeiro, dataDeRecebimento, valor);
+            return true;
         }
     }
 
@@ -222,11 +294,11 @@ namespace PeladAdmin.TestesDeUnidade
 
     public class Caixa
     {
-        public Caixa(string nome, decimal valorMensalidade, Vigencia vigencia)
+        public Caixa(string nome, decimal valorMensalidade, VigenciaDoCaixa vigenciaDoCaixa)
         {
             Nome = nome;
             ValorMensalidade = valorMensalidade;
-            Vigencia = vigencia;
+            VigenciaDoCaixa = vigenciaDoCaixa;
             LancamentosPagamentos = new List<LancamentoPagamento>();
             LancamentosRecebimentos = new List<LancamentoRecebimento>();
         }
@@ -235,51 +307,83 @@ namespace PeladAdmin.TestesDeUnidade
         public IEnumerable<LancamentoPagamento> LancamentosPagamentos { get; set; }
         public string Nome { get; private set; }
         public decimal ValorMensalidade { get; private set; }
-        public Vigencia Vigencia { get; private set; }
-    }
+        public VigenciaDoCaixa VigenciaDoCaixa { get; private set; }
 
-    public class LancamentoPagamento
-    {
-        public LancamentoPagamento(Time time, Caixa caixa, DateTime data, decimal valor)
+        public void FazerLancamentoDeRecebimento(Peladeiro peladeiro, DateTime data, decimal valor)
         {
-            Id = int.MaxValue;
-            Time = time;
-            Caixa = caixa;
-            Data = data;
-            Valor = -valor;
+            ((List<LancamentoRecebimento>)LancamentosRecebimentos).Add(new LancamentoRecebimento(peladeiro, data, valor));
         }
 
-        public int Id { get; private set; }
-        public Time Time { get; private set; }
-        public Caixa Caixa { get; private set; }
-        public DateTime Data { get; private set; }
-        public decimal Valor { get; private set; }
+        public void FazerLancamentoDePagamento(DateTime data, decimal valor)
+        {
+            ((List<LancamentoPagamento>)LancamentosPagamentos).Add(new LancamentoPagamento(data, valor));
+        }
+
+        public decimal CalculaQuantoDeveSerPagoNoPeriodo(DateTime dataInicial, DateTime dataFinal)
+        {
+            return dataInicial.CalcularQuantidadeDeMeses(dataFinal)*ValorMensalidade;
+        }
+
+        public decimal CalculaValorPagoPeloPeladeiroComBaseNaDataDeReferencia(Peladeiro peladeiro, DateTime dataDeReferencia)
+        {
+            return LancamentosRecebimentos.Where(x => x.Peladeiro.Equals(peladeiro) &&
+                    x.Data.Date <= dataDeReferencia).Sum(x => x.Valor);
+        }
     }
 
-    public class LancamentoRecebimento
+    public abstract class LancamentoBase 
     {
-        public LancamentoRecebimento(Time time, Caixa caixa, Peladeiro peladeiro, DateTime data, decimal valor)
+        protected LancamentoBase(DateTime data, decimal valor)
         {
-            Id = int.MaxValue;
-            Time = time;
-            Caixa = caixa;
-            Peladeiro = peladeiro;
             Data = data;
             Valor = valor;
         }
 
-        public int Id { get; private set; }
-        public Time Time { get; private set; }
-        public Caixa Caixa { get; private set; }
-        public Peladeiro Peladeiro { get; private set; }
         public DateTime Data { get; private set; }
         public decimal Valor { get; private set; }
     }
 
-    public class Vigencia
+    public class LancamentoPagamento : LancamentoBase //objeto de valor
     {
-        public Vigencia(DateTime inicio, DateTime fim)
+        public LancamentoPagamento(DateTime data, decimal valor):base(data, valor)
         {
+        }
+    }
+
+    public class LancamentoRecebimento : LancamentoBase //objeto de valor
+    {
+        public LancamentoRecebimento(Peladeiro peladeiro, DateTime data, decimal valor):base(data, valor)
+        {
+            Peladeiro = peladeiro;
+        }
+
+        public Peladeiro Peladeiro { get; private set; }
+    }
+
+    public class VigenciaDoCaixa //objeto de valor
+    {
+        public VigenciaDoCaixa(DateTime inicio, DateTime fim)
+        {
+            Inicio = inicio;
+            Fim = fim;
+        }
+
+        public DateTime Inicio { get; private set; }
+        public DateTime Fim { get; private set; }
+
+        public DateTime PegaDataInicioDeVigenciaConformeDataInicioDoPeladeiro(Peladeiro peladeiro)
+        {
+            return peladeiro.DataDeInicio.Date > Inicio
+                ? peladeiro.DataDeInicio
+                : Inicio;
+        }
+    }
+
+    public static class ExtensoesDeDateTime
+    {
+        public static int CalcularQuantidadeDeMeses(this DateTime de, DateTime ate)
+        {
+            return (ate.Month - de.Month) + 1;//todo: pensar em uma solução melhor
         }
     }
 }
